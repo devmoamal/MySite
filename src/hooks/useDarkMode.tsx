cfe80d94d4d721eb6a72745438f30f68ce413191
Moamal Hussin <dev.moamal@gmail.com>
2026-01-14 22:08:04 +0300

    refactor: Implement dom changing when toggle the dark mode .

--- src/hooks/useDarkMode.tsx
+++ src/hooks/useDarkMode.tsx
@@ -1,8 +1,15 @@
 import useThemeStore from "@/stores/ThemeStore";
+import { useEffect } from "react";
 
 function useDarkMode() {
   const theme = useThemeStore((state) => state.theme);
   const toggle = useThemeStore((state) => state.toggle);
+
+  // Changing dom to the theme
+  useEffect(() => {
+    document.documentElement.className = theme;
+    document.documentElement.style.backgroundColor = "var(--background)";
+  }, [theme]);
 
   return { theme, toggle };
 }
