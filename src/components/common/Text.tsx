b9342ba789f71753667b77d0072105c0b882384a
Moamal Hussin <dev.moamal@gmail.com>
2026-01-14 21:57:56 +0300

    feat: Create Text common component

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -1,12 +1,40 @@
 import { cn } from "@/lib/utils";
 
 type TextProps = {
+  size?: "xs" | "sm" | "md" | "lg";
+  weight?: "normal" | "medium" | "bold";
+  link?: string;
+
   children?: React.ReactNode;
   className?: string;
 };
 
-function Text({ children, className }: TextProps) {
-  return <div className={cn("text-text", className)}>{children}</div>;
+function Text({
+  size = "md",
+  weight = "normal",
+  link,
+  children,
+  className,
+}: TextProps) {
+  return (
+    <p
+      className={cn(
+        size === "xs" && "text-xs",
+        size === "sm" && "text-sm",
+        size === "md" && "text-md",
+        size === "lg" && "text-lg",
+        weight === "normal" && "font-normal",
+        weight === "medium" && "font-medium",
+        weight === "bold" && "font-bold",
+
+        link ? "cursor-pointer underline" : "",
+
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
 }
 
 export default Text;
