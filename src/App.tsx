5d91d1b0e75c918b43da902de9d6b0146d342a05
Moamal Hussin <dev.moamal@gmail.com>
2026-01-14 22:09:44 +0300

    refactor: Implement Navbar & Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
 import useDarkMode from "./hooks/useDarkMode";
 import { cn } from "./lib/utils";
 import Home from "@/pages/Home";
-
 function App() {
   const { theme } = useDarkMode();
+
   return (
-    <div className={cn("dark bg-background h-screen", theme)}>
-      <Home />
+    <div className={cn(theme)}>
+      <Navbar />
+      <div className={cn("bg-background h-screen mt-8", theme)}>
+        <Home />
+      </div>
+      <Footer />
     </div>
   );
 }
