bdc4bf57019bea0d566f001b0ee7144d5c3d573a
Moamal Hussin <dev.moamal@gmail.com>
2026-01-14 22:04:47 +0300

    feat: Creating about section

---
+++ src/sections/Home/About.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils";
+import Text from "@/components/common/Text";
+import Avatar from "@/components/common/Avatar";
+import IconButton from "@/components/common/IconButton";
+import { Github, Instagram, Send } from "lucide-react";
+
+import myPhoto from "@/assets/myPhoto.jpg";
+
+type AboutProps = {
+  className?: string;
+};
+
+function About({ className }: AboutProps) {
+  return (
+    <div className={cn("flex flex-col ", className)}>
+      <Avatar size="md" src={myPhoto} />
+      <Text className="text-text" weight="bold">
+        Moamal Hussin
+      </Text>
+      <Text className="text-text mb-4">Full Stack Developer</Text>
+
+      <div className="flex gap-x-2">
+        {/* Github */}
+        <IconButton
+          size={18}
+          Icon={Github}
+          onClick={() => window.open("https://github.com/devmoamal")}
+        />
+
+        {/* Instagram */}
+        <IconButton
+          size={18}
+          Icon={Instagram}
+          onClick={() => window.open("https://instagram.com/mqgx")}
+        />
+
+        {/* Telegram */}
+        <IconButton
+          size={18}
+          Icon={Send}
+          onClick={() => window.open("https://t.me/moamalhussin")}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default About;
